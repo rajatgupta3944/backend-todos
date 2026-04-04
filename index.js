421bd5711e168fa7a7e8f59fcff6bee1c9f35ac5
Rajat Gupta <rajatgupta3944@gmail.com>
2026-04-05 00:59:18 +0530

    todo endpoints created

---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const {authMiddleware} = require("./middleware.js");
+const jwt = require("jsonwebtoken");
+const app = express();
+app.use(express.json());
+
+let CURRENT_USER_ID = 1;
+let CURRENT_TODO_ID = 1;
+
+let USERS = [];
+let TODOS = [];
+
+app.post("/signup", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const existingUser = USERS.find(u => u.username === username);
+  if(existingUser){
+    res.status(403).json({
+      message: "User with this username already exists!"
+    })
+    return;
+  }
+  USERS.push({
+    id: CURRENT_USER_ID++,
+    username,
+    password,
+  })
+res.json({
+  id: CURRENT_USER_ID - 1
+})
+})
+
+app.post("/signin", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const userExists = USERS.find(u => u.username === username && u.password === password);
+  if(!userExists){
+    res.status(403).json({
+      message: "Incorrect credentials!"
+    })
+  }
+  const token = jwt.sign({
+    userId: userExists.id
+  }, "rajat123");
+  res.json({
+    token
+  })
+})
+
+// Authenticated
+
+app.post("/todo", authMiddleware, (req, res) => { 
+  const userId = req.userId;
+  const title = req.body.title;
+  const description = req.body.description;
+  TODOS.push({
+    id: CURRENT_TODO_ID++,
+    title,
+    description,
+    userId
+  })
+  res.json({
+    message: "Todo created!"
+  })
+})
+
+app.delete("/todo/:todoId", authMiddleware, (req, res) => {
+  const userId = req.userId;
+  const todoId = parseInt(req.params.todoId);
+
+  const doesUserOwnTodo = TODOS.find(t => t.id === todoId && t.userId === userId);
+  if(doesUserOwnTodo){
+    TODOS = TODOS.filter(t => t.id !== userId);
+    res.json({
+      message: "Todo deleted!"
+    })
+  }
+  else{
+    res.json({
+      message: "Either todo doesn't exist or this is not your todo!"
+    })
+  }
+})
+
+app.get("/todos", authMiddleware, (req, res) => {
+  const userId = req.userId;
+  const userTodos = TODOS.filter(t => t.userId === userId);
+  res.json({
+    todos: userTodos
+  })
+})
+
+app.listen(3000);
