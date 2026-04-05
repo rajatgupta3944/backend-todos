67a7fe8dc1448a92196d2a1f43cd3f6824ad58cb
Rajat Gupta <rajatgupta3944@gmail.com>
2026-04-05 12:50:34 +0530

    migrated signup and signin from in memory to mongoDB

--- index.js
+++ index.js
@@ -1,40 +1,54 @@
 const express = require("express");
 const {authMiddleware} = require("./middleware.js");
 const jwt = require("jsonwebtoken");
+const {todoModel, userModel} = require("./models");
+
 const app = express();
 app.use(express.json());
 
-let CURRENT_USER_ID = 1;
-let CURRENT_TODO_ID = 1;
+// let CURRENT_USER_ID = 1;
+// let CURRENT_TODO_ID = 1;
 
-let USERS = [];
-let TODOS = [];
+// let USERS = [];
+// let TODOS = [];
 
-app.post("/signup", (req, res) => {
+app.post("/signup", async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  const existingUser = USERS.find(u => u.username === username);
+  // const existingUser = USERS.find(u => u.username === username);
+  const existingUser = await userModel.findOne({
+    username: username,
+    password: password,
+  })
+  if (existingUser){
+    res.status(403).json({
+      message: "User with this username already exists!"
+    })
+    return;
+  }
   if(existingUser){
     res.status(403).json({
       message: "User with this username already exists!"
     })
     return;
   }
-  USERS.push({
-    id: CURRENT_USER_ID++,
-    username,
-    password,
+  const newUser = userModel.create({
+    username: username,
+    password: password
   })
 res.json({
-  id: CURRENT_USER_ID - 1
+  id: newUser._id
 })
 })
 
-app.post("/signin", (req, res) => {
+app.post("/signin", async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-  const userExists = USERS.find(u => u.username === username && u.password === password);
+  const userExists = await userModel.findOne({
+    username: username,
+    password: password,
+  })
   if(!userExists){
     res.status(403).json({
       message: "Incorrect credentials!"
